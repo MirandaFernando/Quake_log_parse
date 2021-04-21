76e71a989b0c2a2601138cc45bb15e1c43adfbff
Fernando <fernandomirandalolzeiro@gmail.com>
2021-04-20 21:10:23 -0300

    finalizado atualização

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from parse import parse_jogos
 
 
 if __name__ == '__main__':
-    jogos = parse_jogos('games.log')
+    jogos = parse_jogos('teste.log')
     
     for j in jogos:
         print(j)
-    
+        j.ranking_kills_game()
+        j.mortes_list()
 
         
 
