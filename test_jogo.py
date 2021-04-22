32f84e51ab9eccb8b8a536db3d2b5f3d75b67c63
Fernando <fernandomirandalolzeiro@gmail.com>
2021-04-22 10:30:58 -0300

    Adicionado o setUp() no test

--- test_jogo.py
+++ test_jogo.py
@@ -5,11 +5,12 @@ from parse import parse_jogos
 
 
 class TestJogo(unittest.TestCase):
-   
+    def setUp(self):
+        self.jogos = parse_jogos('teste.log')
+
     def test_ranking_kills_game(self):
-        jogos = parse_jogos('teste.log')
-        jogo1 = jogos[0]
-        jogo2 = jogos[1]
+        jogo1 = self.jogos[0]
+        jogo2 = self.jogos[1]
         result = jogo1.ranking_kills_game()
         dict_test = {'Mocinha': 0, 'Isgalamido': -5}
         dict_test2 = {'Isgalamido': 1, 'Dono da Bola': -1, 'Zeh': -2}
@@ -20,9 +21,8 @@ class TestJogo(unittest.TestCase):
         
 
     def test_mortes_list(self):
-        jogos = parse_jogos('teste.log')
-        jogo1 = jogos[0]
-        jogo2 = jogos[1]
+        jogo1 = self.jogos[0]
+        jogo2 = self.jogos[1]
         result = jogo1.mortes_list()
         dict_test = {'MOD_TRIGGER_HURT': 7, 'MOD_ROCKET_SPLASH': 3, 'MOD_FALLING': 1}
         dict_test2 = {'MOD_TRIGGER_HURT': 2, 'MOD_ROCKET': 1, 'MOD_FALLING': 1}
@@ -32,16 +32,14 @@ class TestJogo(unittest.TestCase):
         #self.assertDictEqual(result, dict_test)
     
     def test_jogo_id(self):
-        jogos = parse_jogos('teste.log')
-        jogo1 = jogos[0]
-        jogo2 = jogos[1]
+        jogo1 = self.jogos[0]
+        jogo2 = self.jogos[1]
         self.assertEqual(jogo1.jogo_id(), 1)
         self.assertEqual(jogo2.jogo_id(), 2)
     
     def test_quant_kills(self):
-        jogos = parse_jogos('teste.log')
-        jogo1 = jogos[0]
-        jogo2 = jogos[1]
+        jogo1 = self.jogos[0]
+        jogo2 = self.jogos[1]
         self.assertEqual(jogo1.quant_kills(), 11)
         self.assertEqual(jogo2.quant_kills(), 4)
 
